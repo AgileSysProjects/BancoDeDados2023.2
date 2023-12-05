9a677dd4b95be445e46810639ddcf925decc3708
SAndrade100 <andradedearaujo100@gmail.com>
2023-12-05 09:46:31 -0300

    Correções.

--- Scripts/Consultas/12 - DisciplinasSemAvaliacao.sql
+++ Scripts/Consultas/12 - DisciplinasSemAvaliacao.sql
@@ -1,4 +1,5 @@
 -- Retorna as disciplinas que não lançaram avaliações
+-- Todas possuem avaliações, por isso retorna uma tabela vazia
 SELECT Nome
 FROM Disciplinas D
 WHERE NOT EXISTS (
--- Scripts/Consultas/4 - FaltaEndereco.sql
+++ Scripts/Consultas/4 - FaltaEndereco.sql
@@ -1,4 +1,5 @@
 -- Retorna professores que não possuem endereço cadastrado
+-- Todos os professores tem endereço cadastrado, por isso o resultado é uma tabela vazia
 SELECT professores.matricula, professores.nome
 FROM professores
 LEFT JOIN enderecoprofessor ON professores.matricula = enderecoprofessor.matriculaprofessor
